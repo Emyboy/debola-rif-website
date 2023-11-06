695ebd4623546a6ae8570bded1c7879da9de258e
olawoyin Daniel <olawoyindaniel95@gmail.com>
2023-11-06 09:58:57 +0100

    made a change

--- src/components/pages/HomePage/HeroSection.jsx
+++ src/components/pages/HomePage/HeroSection.jsx
@@ -1,11 +1,8 @@
-import ContainLayout from '@/components/layout/ContainerLayout'
-import Link from 'next/link'
-import React,{useEffect,useRef} from 'react'
+import ContainLayout from '@/components/layout/ContainerLayout';
+import Link from 'next/link';
+import React, { useEffect, useRef } from 'react';
 
 const HeroBanner = ({ title, subtitle, children, description }) => {
-
-
-
   return (
     <div
       className=' h-[100%]  w-[100%]  bg-cover  bg-left md:h-[100vh] md:w-[100%] '
@@ -16,12 +13,10 @@ const HeroBanner = ({ title, subtitle, children, description }) => {
       <div className='h-full w-full backdrop-brightness-50'>
         <ContainLayout>
           <div className='flex flex-col py-[18rem]'>
-            <h3 className='font-museo text-[50px] font-bold text-white' >
+            <h3 className='font-poppins text-[50px] font-bold text-white'>
               RIF- ROYAL IWERE FOUNDATION
             </h3>
-            <p className='text-[20px] text-white'>
-              An Iwere Nation's Human Capital Development Programme
-            </p>
+            <p className='text-[20px] text-white'>Educate. Enlighten. Empowr</p>
             <button className='mt-6 h-[40px] w-fit rounded-lg  bg-[rgb(0,104,55)] px-12 text-[1.2rem] font-bold text-white'>
               <Link href='/contact-us'> Contact us </Link>
             </button>
